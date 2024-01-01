17c20cfe57df35a4bba38e89701185922f4a5124
KelvinOjedaEpn <kelvin.ojeda@epn.edu.ec>
2024-01-01 09:00:58 -0500

    Mod: totalExpensesPorcentage solved to not show NaN in ExpenseChart.jsx

--- src/components/ExpenseChart.jsx
+++ src/components/ExpenseChart.jsx
@@ -3,10 +3,6 @@ import {useGlobalState} from "../context/GlobalState"
 
 function ExpenseChart() {
   const {transactions} = useGlobalState()
-  const total = transactions.reduce(
-    (acc, transaction) => (acc += transaction.amount),
-    0
-  )
   const expense =
     transactions
       .filter((transaction) => transaction.amount < 0)
@@ -21,8 +17,8 @@ function ExpenseChart() {
     <VictoryPie
       colorScale={["#e74c3c", "#2ecc71"]}
       data={[
-        {x: "Expenses", y: totalExpensesPorcentage},
-        {x: "Incomes", y: totalIncomePorcentage},
+        {x: "Expenses", y: totalExpensesPorcentage ? totalExpensesPorcentage : 0},
+        {x: "Incomes", y: totalIncomePorcentage ? totalIncomePorcentage: 0},
       ]}
       animate={{
         duration: 200,
